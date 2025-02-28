fadefe0b67d5be43965ac8582735a70aa2dce79c
Edin Adolfo <emartinezs19@miumg.edu.gt>
2025-02-28 17:36:25 -0600

    Subir codigo sin archivos temporales

---
+++ Proyecto/Parte1/Desafio3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Proyecto.Parte1
+{
+    public class Desafio3
+    {
+        static void Sumar()
+        {
+            try
+            {
+                Console.WriteLine("Ingrese el primer número:");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el segundo número:");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Suma: " + (n1 + n2));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: Ingresa solo números enteros.");
+            }
+            finally
+            {
+                Console.WriteLine("Fin del programa.");
+            }
+        }
+
+        public static void Ejecutar()
+        {
+            Sumar();
+        }
+    }
+}
---
+++ Proyecto/Parte2/Desafio1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Proyecto.Parte2
+{
+    public class Desafio1
+    {
+        public static void Ejecutar()
+        {
+            string usuarioRegistrado = "";
+            string contraseñaRegistrada = "";
+
+            Console.WriteLine("Registrarse");
+            Console.Write("Ingrese un nombre de usuario: ");
+            usuarioRegistrado = Console.ReadLine();
+
+            Console.Write("Ingrese una contraseña: ");
+            contraseñaRegistrada = Console.ReadLine();
+
+            Console.WriteLine("¡Registro exitoso!");
+
+            Console.WriteLine("\nIniciar sesión");
+            Console.Write("Ingrese su nombre de usuario: ");
+            string usuarioIngresado = Console.ReadLine();
+
+            Console.Write("Ingrese su contraseña: ");
+            string contraseñaIngresada = Console.ReadLine();
+
+            if (usuarioIngresado == usuarioRegistrado && contraseñaIngresada == contraseñaRegistrada)
+            {
+                Console.WriteLine("Inicio de sesión exitoso. ¡Bienvenido " + usuarioIngresado + "!");
+            }
+            else
+            {
+                Console.WriteLine("Nombre de usuario o contraseña incorrectos.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
